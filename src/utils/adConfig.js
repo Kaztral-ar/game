593f0b0f3c25af2d8fc405db6d3006ed049067ec
Gokul A R <gokullogin46@gmail.com>
2026-09-02 07:13:30 +0530

    Configure production AdMob banner unit

--- src/utils/adConfig.js
+++ src/utils/adConfig.js
@@ -1,19 +1,23 @@
 import { TestIds } from 'react-native-google-mobile-ads';
 
+// Production AdMob configuration.
+// The supplied ad unit is configured as the banner unit.
 const PRODUCTION_IDS = {
-  banner: '',
+  banner: 'ca-app-pub-6906509746436244/2220867988',
   interstitial: '',
   rewarded: '',
 };
 
-export const AD_UNIT_IDS = __DEV__ ? {
-  banner: TestIds.BANNER,
-  interstitial: TestIds.INTERSTITIAL,
-  rewarded: TestIds.REWARDED,
-} : {
-  banner: PRODUCTION_IDS.banner || TestIds.BANNER,
-  interstitial: PRODUCTION_IDS.interstitial || TestIds.INTERSTITIAL,
-  rewarded: PRODUCTION_IDS.rewarded || TestIds.REWARDED,
-};
+export const AD_UNIT_IDS = __DEV__
+  ? {
+      banner: TestIds.BANNER,
+      interstitial: TestIds.INTERSTITIAL,
+      rewarded: TestIds.REWARDED,
+    }
+  : {
+      banner: PRODUCTION_IDS.banner || TestIds.BANNER,
+      interstitial: PRODUCTION_IDS.interstitial || TestIds.INTERSTITIAL,
+      rewarded: PRODUCTION_IDS.rewarded || TestIds.REWARDED,
+    };
 
 export { PRODUCTION_IDS };
